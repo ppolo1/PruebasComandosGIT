33804eb3472afa7284bd7a918fe447369099bee5
ppolo1 <pppcuenca@gmail.com>
2024-04-11 10:33:17 +0200

    primer cambio

---
+++ primera_clase.java
@@ -0,0 +1,10 @@
+
+public class primera_clase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola que tal");
+		System.out.println("Esta es la primera clase");
+	}
+
+}
